510d58e92c290fb6a9de7f51a7bbff98bfc12c06
yeh1 <yeh1@rose-hulman.edu>
2018-10-09 22:00:39 -0400

    Test 2

--- src/problem1.py
+++ src/problem1.py
@@ -2,8 +2,8 @@
 Exam 2, problem 1.
 
 Authors: David Mutchler, Dave Fisher, Matt Boutell, their colleagues,
-         and PUT_YOUR_NAME_HERE.  October 2018.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         and Hengqi Ye(Luis).  October 2018.
+"""  # Done: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import time
 import testing_helper
@@ -48,8 +48,8 @@ def main():
     print('Un-comment the calls in MAIN one by one')
     print(' to run the testing code as you complete the TODOs.')
 
-    # run_test_problem1a()
-    # run_test_problem1b()
+    run_test_problem1a()
+    run_test_problem1b()
     # run_test_problem1c()
     # run_test_problem1d()
 
@@ -218,9 +218,18 @@ def problem1a(strings):
       :rtype: list of int
     """
     # -------------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # Done: 2. Implement and test this function.
     #          Tests have been written for you (above).
     # -------------------------------------------------------------------------
+    s = []
+    print(len(strings))
+    for k in range(len(strings)):
+        q = strings[k]
+        s += [len(q)]
+
+    return s
+
+
 
 
 def run_test_problem1b():
@@ -391,9 +400,16 @@ def problem1b(strings):
       :rtype: int
     """
     # -------------------------------------------------------------------------
-    # TODO: 3. Implement and test this function.
+    # Done: 3. Implement and test this function.
     #          Tests have been written for you (above).
     # -------------------------------------------------------------------------
+    s = 0
+
+    for k in range(len(strings)):
+        q = strings[k]
+        if is_prime(len(q)):
+            s = s + 1
+    return s
 
 
 def run_test_problem1c():
